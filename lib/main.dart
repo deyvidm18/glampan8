4a1abb688626757b65f47263474ef51c5f3c3036
Deyvid Martinez <deyvidm18@gmail.com>
2019-02-13 21:46:44 -0600

    Initial Commit - Firebase Integration

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:glampan8/pages/root_page.dart';
+import 'package:glampan8/services/authentication.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
